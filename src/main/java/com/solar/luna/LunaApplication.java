e89f72899370e9654e3ab28096b49e4b8c5fe087
Huang Zhibo <zhibohuang@creditease.cn>
2019-08-07 14:06:11 +0800

    project init

---
+++ src/main/java/com/solar/luna/LunaApplication.java
@@ -0,0 +1,17 @@
+package com.solar.luna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author huangzhibo
+ * @date 2019-08-06
+ */
+@SpringBootApplication
+public class LunaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LunaApplication.class, args);
+    }
+
+}
