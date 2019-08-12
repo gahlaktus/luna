8a5cabdfe037af7301de6e496c07595955c2cf28
Huang Zhibo <zhibohuang@creditease.cn>
2019-08-12 10:50:57 +0800

    builder模式

---
+++ src/main/java/com/solar/luna/feign/demo/Github.java
@@ -0,0 +1,8 @@
+package com.solar.luna.feign.demo;
+
+/**
+ * @author huangzhibo
+ * @date 2019-08-09
+ */
+public interface Github {
+}
---
+++ src/main/java/com/solar/luna/patterndesign/builder/Person.java
@@ -0,0 +1,8 @@
+package com.solar.luna.patterndesign.builder;
+
+/**
+ * @author huangzhibo
+ * @date 2019-08-09
+ */
+public class Person {
+}
